2ff544a15ff0f6c2412f225f7a49ff7602fa28ff
AlexaEgorova <37927173+AlexaEgorova@users.noreply.github.com>
2020-05-16 16:59:01 +0300

    ne buzi, Hleb

--- Speech2Text.py
+++ Speech2Text.py
@@ -1,29 +1,22 @@
 import speech_recognition as sr
 
-class Speecher():
+
+class Speecher:
     def __init__(self):
         self.recognizer = sr.Recognizer()
         self.speech = ""
 
-    def listenTo(self):
+    def listen_to(self):
         with sr.Microphone() as source:
             print("Say something!")
             audio = self.recognizer.listen(source)
         try:
-            # for testing purposes, we're just using the default API key
-            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-            # instead of `r.recognize_google(audio)`
             self.speech = self.recognizer.recognize_google(audio)
             print("Google Speech Recognition thinks you said " + self.speech)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            print("Could not request results; {0}".format(e))
 
         print("Returned " + self.speech)
         return self.speech
-
-
-spchr = Speecher()
-spchr.listenTo()
-
